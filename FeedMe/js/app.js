c46a4013102b7b5e300241398b1f4db18b30450c
Mark Sweet <marks828@gmail.com>
2016-10-31 16:11:10 -0400

    map functioning

--- FeedMe/js/app.js
+++ FeedMe/js/app.js
@@ -14,10 +14,34 @@ angular
     FeedMeShow
   ])
 
+  function mapFunction(){
+    console.log("here");
+    // mapboxgl.accessToken = 'pk.eyJ1IjoibWFya3M4MjgiLCJhIjoiY2l1dTZ0eG9vMDJhMzJ5b2VwdWpjbHJmeSJ9.pI-acZvMrbtOHhfSaui34Q';
+    // var map = new mapboxgl.Map({
+    //     container: 'map',
+    //     style: 'mapbox://styles/mapbox/streets-v9',
+    //     center: [-79.4512, 43.6568],
+    //     zoom: 13
+    // });
+    //
+    // map.addControl(new mapboxgl.Directions());
+    var mymap = L.map('map').setView([30.263595, -97.762695], 15);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'marks828.207gggg3',
+      accessToken: 'pk.eyJ1IjoibWFya3M4MjgiLCJhIjoiY2l1dTZ0eG9vMDJhMzJ5b2VwdWpjbHJmeSJ9.pI-acZvMrbtOHhfSaui34Q'
+    })
+
+    .addTo(mymap);
+  }
+
   function FeedMeIndex(){
     console.log("hello")
   }
   function FeedMeShow(){
+    mapFunction()
     console.log("This is the show page!")
   }
 
