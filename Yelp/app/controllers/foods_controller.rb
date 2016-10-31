03a80f591b6e14883c6265c7cda31690e8bb8cdc
Michael Lai <mike24567@gmail.com>
2016-10-31 14:06:05 -0400

    able to retrieve hardcoded show

--- Yelp/app/controllers/foods_controller.rb
+++ Yelp/app/controllers/foods_controller.rb
@@ -2,11 +2,18 @@ class FoodsController < ApplicationController
 
   def index
     @foods = Food.all
+    render json: @foods
   end
+
+  # def search(location)
+  #   params.merge!({location: location})
+  #
+  #   Response::Search.new(JSON.parse(search_request(params).body))
+  # end
 
   def show
     @food = Food.find(params[:id])
-    @params = {term: 'food', limit: 16}
+    @params = {term: 'food', limit: 20}
     @results = render json: Yelp.client.search(@food.location, @params )
     puts @food
   end
