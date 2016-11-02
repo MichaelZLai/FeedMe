976c58ea432d8732bceaca54f8ad64684aa65712
Michael Lai <mike24567@gmail.com>
2016-11-02 11:51:45 -0400

    working on incrementing counter

--- Yelp/app/controllers/visits_controller.rb
+++ Yelp/app/controllers/visits_controller.rb
@@ -2,9 +2,20 @@ class VisitsController < ApplicationController
 
 
   def create
-    @visit = Visit.create!(visit_params)
-    render json: @visit
+    @visit = Visit.increment!(:count).find_or_create_by(visit_params)
+    # @visit.save!
+
     puts @visit
+
+  #   if @visit.exists?
+  #     @visit.count += 1
+  #     @visit.save!
+  #     render json: @visit
+  #   else
+  #     @visit = Visit.create!(visit_params)
+  #     render json: @visit
+  #     puts @visit
+  #   end
   end
 
  private
